ac58475734767e1adbbf4d3041ba977fbfd750a3
Arentrir <jorkodaskalovi1@gmail.com>
2025-12-01 06:37:38 +0200

    Initial commit

--- FitnessBookingSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FitnessBookingSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
